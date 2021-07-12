c09dd41b14369cc60e007d76f777a4d6ef08147d
Lucas Yule <lyrma@cin.ufpe.br>
2021-07-12 16:07:02 -0300

    Final? commit Desafio-complementar01

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
+import { ReactNode } from "react";
+import { MovieCard } from "./MovieCard";
+
+
+interface IContentProps{
+  genreTitle:string;
+  children:ReactNode;
+}
+
+export function Content(props:IContentProps) {
   // Complete aqui
+  return(
+    <>
+    <header>
+    <span className="category">
+      Categoria:<span> {props.genreTitle}</span>
+    </span>
+  </header>
+
+  <main>
+    <div className="movies-list">
+      {props.children}
+    </div>
+  </main>
+  </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,19 @@
-export function SideBar() {
+import { ReactNode } from "react";
+interface ISideBarProps{
+  children: ReactNode
+}
+
+export function SideBar(props:ISideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.children}
+      </div>
+    </nav>
+  );
 }
